027d57669d25d5fe37faeee4d5cb6761e4fe46e0
matteo todescato <mtodescato@Dell.homenet.telecomitalia.it>
2017-01-05 18:07:58 +0100

    da completare la funzione effetuaLogin con l'effettivo controlle se l'utente con quell'username e password si presente o meno, con il suo ritorno tramite puntatore dalla funzione utentePresente

--- login.cpp
+++ login.cpp
@@ -6,15 +6,32 @@ login::login (const listaUtenti& lst ,QDialog* parent): QDialog(parent),lista(ls
   resize(600,300);
   QPushButton* log = new QPushButton(tr("login"),this);
   log->setMinimumSize(200,50);
-  QLineEdit* userName = new QLineEdit(this);
-  QLineEdit* passwd = new QLineEdit(this);
-  passwd->setEchoMode(QLineEdit::Password);
+  passwd.setEchoMode(QLineEdit::Password);
   QFormLayout* Layout = new QFormLayout(this);
   QHBoxLayout* button = new QHBoxLayout(this);
-  Layout->addRow(tr("username"),userName);
-  Layout->addRow(tr("password"),passwd);
+  Layout->addRow(tr("username"),&userName);
+  Layout->addRow(tr("password"),&passwd);
   button->addWidget(log);
   Layout->addItem(button);
   this->setLayout(Layout);
-
+  connect(log,SIGNAL(clicked()),this,SLOT(effetuaLogin()));
 }
+
+login::~login() {}
+
+
+void login::effetuaLogin() {
+  std::string nome = (userName.text().toStdString());
+  std::string pswd = (passwd.text().toStdString());
+}
+
+
+/*
+void login::eseguiLogin(const std::string& usrn, const std::string& passwd) const {
+    utente log(usrn,passwd);
+    utente* p = lista.utentePresente(log);
+    if(!p) { signal che fa comparire una qdialog con scritto "username o password errati}
+    else
+      {signal che fa aprire il programma con l'interfaccia adatta all'utente }
+}
+*/
--- login.h
+++ login.h
@@ -14,10 +14,13 @@ class login : public QDialog {
   Q_OBJECT
 private:
   listaUtenti lista;
+  QLineEdit userName;
+  QLineEdit passwd;
 public:
   login(const listaUtenti& ,QDialog* =0);
-public slot:
-  void eseguiLogin(const std::string&, const std::string&) const;
+  ~login();
+public slots:
+  void effetuaLogin();
 
 };
 
