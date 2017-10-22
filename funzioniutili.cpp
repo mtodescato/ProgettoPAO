26c9b44db9334a86333cea0de88111fbf9df6d83
Matteo Todescato <mtodescato@DellXps.homenet.telecomitalia.it>
2017-10-22 09:50:05 +0200

    aggiunti i file per i dati e la relazione

---
+++ funzioniutili.cpp
@@ -0,0 +1,18 @@
+#include "funzioniutili.h"
+
+/*
+std::string readXmlString(QXmlStreamReader& reader) {
+  if(reader.readNextStartElement())
+    return (reader.readElementText()).toStdString();
+  else
+    return "";
+}
+
+int readXmlint(QXmlStreamReader& reader) {
+  if(reader.readNextStartElement())
+    return (reader.readElementText()).toInt();
+  else
+    return 0;
+}
+
+*/
---
+++ funzioniutili.h
@@ -0,0 +1,12 @@
+#ifndef FUNZIONIUTILI_H
+#define FUNZIONIUTILI_H
+
+#include <string>
+#include <QXmlStreamReader>
+
+/*
+std::string readXmlString(QXmlStreamReader& );
+int readXmlint(QXmlStreamReader& );
+*/
+
+#endif // FUNZIONIUTILI_H
